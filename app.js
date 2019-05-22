fe67f7ef0c1788a93fef0d0f69d820c94fda0227
minhtu12394 <minhtu12394@gmail.com>
2019-05-22 13:17:31 +0700

    connect to MongoDB

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
+var config = require('./config/database');
+
+//conect to db
+mongoose.connect(config.database);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connect to MongoDB");
+});
 
 // Init app
 var app = express();
 
 // View engine setup
-
 app.set('view', path.join(__dirname, 'view'));
 app.set('view engine', 'ejs');
 
 // Set public folder
-
-app.use(express.static(path.join(__dirname,'public')));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
   res.send('working');
